59ceaf04e0e60a0e03bc7b1d7893b69c5adbc0ea
TECH HUB <giri.techhub23@gmail.com>
2020-08-01 14:38:13 +0530

    this is the demo example of git hub

---
+++ TestCOREGIT/src/org/techhub/Hello.java
@@ -0,0 +1,10 @@
+package org.techhub;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good Morning India");
+	}
+
+}
