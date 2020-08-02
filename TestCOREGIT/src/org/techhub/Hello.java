ef32cb5e45276f040d6311fda7d0a7e9a99795d5
TECH HUB <giri.techhub23@gmail.com>
2020-08-02 14:33:44 +0530

    this is the feature branch

--- TestCOREGIT/src/org/techhub/Hello.java
+++ TestCOREGIT/src/org/techhub/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Good Morning India");
+		System.out.println("testing");
 	}
 
 }
